84fd1233fa9f13286f8381143a04f4cb9ea06373
Jack Styles <jackstyles320@gmail.com>
2023-08-11 14:28:04 +0100

    users: Add users model
    This also includes all the fields that are required
    for the database table
    
    Signed-off-by: Jack Styles <jackstyles320@gmail.com>

--- assignment/assignment/myapp/models.py
+++ assignment/assignment/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    user_id = models.IntegerField(primary_key=True, null=False)
+    group_name = models.CharField(max_length=50)
+    date_joined = models.DateField(null=False)
+    email_address = models.CharField(max_length=100, null=False)
+    password = models.CharField(max_length=1000, null=False)
