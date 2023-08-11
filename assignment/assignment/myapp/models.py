ca726ebc73c56007ecf6deb6ec8424f5f0e1c666
Jack Styles <jackstyles320@gmail.com>
2023-08-11 14:43:04 +0100

    tickets: Add Tickets model object
    This also adds in the attributes of the table
    
    Signed-off-by: Jack Styles <jackstyles320@gmail.com>

--- assignment/assignment/myapp/models.py
+++ assignment/assignment/myapp/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 class Users(models.Model):
     user_id = models.IntegerField(primary_key=True, null=False)
-    group_name = models.CharField(max_length=50)
+    group_id = models.IntegerField()
     date_joined = models.DateField(null=False)
     email_address = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=1000, null=False)
+
+class Tickets(models.Model):
+    ticket_id = models.IntegerField(primary_key=True, null=False)
+    ticket_details = models.CharField(max_length=100)
+    ticket_description = models.CharField(max_length=2000)
+    assignee_user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
