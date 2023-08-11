95cf8f1c123526e9f336b183cee53d3b16b9de42
Jack Styles <jackstyles320@gmail.com>
2023-08-11 14:57:02 +0100

    Users: Change group_id to use UserGroups as a Foreign Key
    This also requires there to be a reorder of the order the
    models are defined in, otherwise the UserGroups class
    is not available to use.
    
    Signed-off-by: Jack Styles <jackstyles320@gmail.com>

--- assignment/assignment/myapp/models.py
+++ assignment/assignment/myapp/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class UserPermissions(models.Model):
+    permissions_id = models.IntegerField(primary_key=True, mull=False)
+    persmission_name = models.CharField(max_length=50, null=False)
+
+class UserGroups(models.Model):
+    group_id = models.IntegerField(primary_key=True, null=False)
+    group_name = models.CharField(max_length=50, null=False)
+    permissions_id = models.ForeignKey(UserPermissions, on_delete=models.CASCADE)
+
 class Users(models.Model):
     user_id = models.IntegerField(primary_key=True, null=False)
-    group_id = models.IntegerField()
+    group_id = models.ForeignKey(UserGroups, on_delete=models.CASCADE)
     date_joined = models.DateField(null=False)
     email_address = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=1000, null=False)
@@ -12,12 +21,3 @@ class Tickets(models.Model):
     ticket_details = models.CharField(max_length=100)
     ticket_description = models.CharField(max_length=2000)
     assignee_user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-
-class UserPermissions(models.Model):
-    permissions_id = models.IntegerField(primary_key=True, mull=False)
-    persmission_name = models.CharField(max_length=50, null=False)
-
-class UserGroups(models.Model):
-    group_id = models.IntegerField(primary_key=True, null=False)
-    group_name = models.CharField(max_length=50, null=False)
-    permissions_id = models.ForeignKey(UserPermissions, on_delete=models.CASCADE)
