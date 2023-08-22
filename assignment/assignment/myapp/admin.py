dff3d2543c1b784612b9aa0c26e7b5d760484fa1
Jack Styles <jackstyles320@gmail.com>
2023-08-22 16:03:46 +0100

    admin: add models to the admin page
    This allos for the models to be viewed and manipulated
    from the admin page
    
    Signed-off-by: Jack Styles <jackstyles320@gmail.com>

--- assignment/assignment/myapp/admin.py
+++ assignment/assignment/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from myapp.models import UserGroups, UserPermissions, Users, Tickets
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(UserGroups)
+admin.site.register(UserPermissions)
+admin.site.register(Tickets)
