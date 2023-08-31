c36e14eabc63f4f5a3b69fa3c9f33553fcf90bbd
Jack Styles <jackstyles320@gmail.com>
2023-08-31 14:16:49 +0100

    data-model: Update Data Model for better visualisation of forms
    Signed-off-by: Jack Styles <jackstyles320@gmail.com>

--- assignment/assignment/myapp/models.py
+++ assignment/assignment/myapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Status(models.Model):
-    status_id = models.AutoField(primary_key=True, null=False)
-    status_description = models.CharField(max_length=50)
+    status_name = models.CharField(max_length = 20, primary_key=True, null=False)
+    status_description = models.CharField(max_length=100, blank=True, null=True)
 
 class TicketType(models.Model):
     type_name = models.CharField(max_length=20, null=False, primary_key=True)
-    type_description = models.CharField(max_length=100, null=False)
+    type_description = models.CharField(max_length=100, blank=True, null=True)
 
 class Ticket(models.Model):
     ticket_id = models.AutoField(primary_key=True, null=False)
     ticket_title = models.CharField(max_length=100, null=False)
-    ticket_info = models.CharField(max_length=2000, null=False)
-    assignee = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
-    type = models.ForeignKey(TicketType, on_delete=models.CASCADE)
+    ticket_info = models.CharField(max_length=2000, blank=True, null=True)
+    assignee = models.ForeignKey(User, blank=True, on_delete=models.CASCADE, null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+    type = models.ForeignKey(TicketType, on_delete=models.CASCADE, null=True)
     date_reported = models.DateField(null=False)
-    date_due = models.DateField()
+    date_due = models.DateField(blank=True, null=True)
