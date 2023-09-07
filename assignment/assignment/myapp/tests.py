369f18809e1497f725db35c970298efc0965d5c9
Jack Styles <99514724+Stylie777@users.noreply.github.com>
2023-09-07 21:32:41 +0100

    models: Add Unit Tests
    This adds Unit Tests for the models that have their own functions. These tests are to ensure that the names returned are correct to improve usability when assigning users and ticket types by the user.

--- assignment/assignment/myapp/tests.py
+++ assignment/assignment/myapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from myapp.models import Status, TicketType
 
-# Create your tests here.
+class TestStatusModel(TestCase):
+    @classmethod
+    def create_status_object(self, status_name="Test Status", status_description="This is a test status"):
+        status_object =  Status.objects.create(status_name=status_name, status_description=status_description)
+        return status_object
+
+    def test_string_creation(self):
+        status = self.create_status_object()
+        status_name = status.__str__()
+        self.assertEqual(status_name, "Test Status")
+
+class TestTicketTypeModel(TestCase):
+    @classmethod
+    def create_ticket_type_object(self, type_name="Test Ticket Type", type_description="This is a test description"):
+        return TicketType.objects.create(type_name=type_name, type_description=type_description)
+
+    def test_string_creation(self):
+        ticket_type = self.create_ticket_type_object()
+        ticket_type_name = ticket_type.__str__()
+        self.assertEqual(ticket_type_name, "Test Ticket Type")
